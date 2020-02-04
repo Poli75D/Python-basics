9b981a90189ac780f536e0fdae0dc2dea5646a50
Poli75D <59068559+Poli75D@users.noreply.github.com>
2020-02-04 21:14:40 +0100

    Add files via upload

---
+++ file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+print('Content-type:text/html\r\n\r\n')
+
+print('<html>')
+
+print('<head>')
+print('<title>My First CGI App!</title>')
+print('</head>')
+
+print('<body>')
+print('<h3>This is HTMLs Body Section!</h3>')
+print('</body>')
+
+print('</html>')
